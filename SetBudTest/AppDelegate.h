8e28ad964ecd2afb08a29bd1e424725bf92720d1
MilanistaDev <milanista224@gmail.com>
2015-01-23 18:46:45 +0900

    Initial Commit

---
+++ SetBudTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SetBudTest
+//
+//  Created by 麻生 拓弥 on 2015/01/23.
+//  Copyright (c) 2015年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SetBudTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SetBudTest
+//
+//  Created by 麻生 拓弥 on 2015/01/23.
+//  Copyright (c) 2015年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
